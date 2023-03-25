b1fd2574f2c99ba48b2ba4d0a4ea2e2946973b5e
Eric Collard <ruffyreborn@gmail.com>
2023-03-25 11:57:20 -0500

    Uploading for troubleshooting

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-console.log('This town ain\'t big \'nuff for the two\'a us. ');
-
 const links = document.querySelectorAll('.navbar-link');
-
 
 links.forEach((link) => {
     link.addEventListener('click', () => {
@@ -10,8 +7,8 @@ links.forEach((link) => {
         link.className = 'navbar-link';
         });
         link.classList.add('change');
-        
-        subLinks.previousElementSibling.classList.add('previousElement');
-        subLinks.nextElementSibling.classList.add('nextElement');
+
     });
 });
+
+
